e6dbcac1fb9919964291463a64d87bcb2e9ae364
蔡庭育 <caitingyu@caitingyudeMac-mini.local>
2026-03-26 18:21:19 +0800

    feat: add Progressive Web App (PWA) support

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "color-memory-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Caching assets...");
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
